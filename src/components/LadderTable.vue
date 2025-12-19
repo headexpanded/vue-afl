3db05f1ee162deff51aafffabbc374fbbcaddea3
Mark Hatherly <mhatherly@mixvoip.com>
2025-12-19 15:02:48 +0100

    rename to LadderTable.vue

---
+++ src/components/LadderTable.vue
@@ -0,0 +1,63 @@
+<template>
+  <table>
+    <thead>
+      <tr class="table-header">
+        <th>#</th>
+        <th>Club</th>
+        <th>P</th>
+        <th>W</th>
+        <th>L</th>
+        <th>D</th>
+        <th>%</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="club in store.ladder" :key="club.id" @click="showClub">
+        <td>{{ club.rank }}</td>
+        <td>{{ club.name }}</td>
+        <td>{{ club.played }}</td>
+        <td>{{ club.wins }}</td>
+        <td>{{ club.losses }}</td>
+        <td>{{ club.draws }}</td>
+        <td>{{ club.percentage }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script setup lang="ts">
+import { useLadderStore } from '@/stores/ladder-store.ts'
+import { onMounted } from 'vue'
+
+/* ============ PROPS ============ */
+
+/* ============ EMITS ============ */
+
+/* ========= LOCAL SCOPE ========= */
+
+const store = useLadderStore()
+
+/* ============= REFS ============ */
+
+/* ============ HOOKS ============ */
+
+onMounted(() => {
+  store.getLadder()
+})
+
+/* =========== METHODS =========== */
+
+const showClub = () => {
+  console.log('Click')
+}
+</script>
+
+<style scoped>
+table {
+  width: 90vw;
+}
+thead {
+  background-color: blue;
+  color: white;
+  height: 40px;
+}
+</style>
